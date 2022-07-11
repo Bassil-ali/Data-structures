054d6ba0fe0099919373cb1fb8d39d4ab2274d81
Bassil-ali <baselali337@gmail.com>
2022-07-11 13:58:17 +0200

    tree

---
+++ tree.py
@@ -0,0 +1,42 @@
+class Node:
+    def __init__(self,item):
+        self.item = item
+        self.left = None
+        self.right = None
+
+
+def sumTree(ele):
+    sum = 0
+    if(ele == None):
+        return 0
+    return (ele.item + sumTree(ele.left) + sumTree(ele.right))
+
+def printTree(ele):
+    if(ele == None):
+        return 0
+    print(ele.item)
+    printTree(ele.left)
+    printTree(ele.right)
+
+# add int to tree
+one = Node(1)
+tow = Node(2)
+three = Node(3)
+four = Node(4)
+fife = Node(5)
+six = Node(6)
+
+#connect tree
+one.left = tow
+one.right = three
+tow.left = four
+tow.right = fife
+three.right = six 
+
+#print tree
+print("tree item is: ")
+printTree(one)
+
+#sum tree
+print("sum tree items is: ")
+print(sumTree(one))
